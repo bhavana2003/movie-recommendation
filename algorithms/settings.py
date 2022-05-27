1728b2d11fc4c64dbeecef5132d0509d81ccb782
bhavana2003 <65390549+bhavana2003@users.noreply.github.com>
2022-05-27 18:22:34 +0530

    third commit

--- algorithms/settings.py
+++ algorithms/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8e@+mnc(0w*5q+xd^b%&j%q%)2v3(=xe^)xd702)j(ultxffs2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','do-recommand.herokuapp.com']
 
